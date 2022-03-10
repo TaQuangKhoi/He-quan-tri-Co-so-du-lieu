f0d9c60b0ddecbea7d04b65078f616eabecc2313
Keios Starqua <khoi0941@gmail.com>
2022-03-10 11:15:31 +0700

    done: #2
    b. Tên các vận động viên của đội số 3 của câu lạc bộ có mã số 27

--- Quản Lý Thể Thao/Query.sql
+++ Quản Lý Thể Thao/Query.sql
@@ -2,3 +2,10 @@
 SELECT VDV_TEN, CLB_MA
 FROM VDV
 WHERE CLB_MA = 45
+
+-- b. Tên các v?n ??ng viên |c?a ??i s? 3| c?a |câu l?c b? có mã s? 27|
+SELECT V.VDV_TEN, V.CLB_MA, VD.DOI_MA
+FROM VDV V, VDV_DOI VD
+WHERE V.CLB_MA = 27
+AND V.VDV_MA = VD.VDV_MA
+AND VD.DOI_MA = 3
