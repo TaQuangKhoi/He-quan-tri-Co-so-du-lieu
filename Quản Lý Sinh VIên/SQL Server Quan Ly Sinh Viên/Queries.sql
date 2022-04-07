bb508a845c185f7026d0b329fd1b0098315f9762
Keios Starqua <khoi0941@gmail.com>
2022-04-07 13:47:32 +0700

    Tách riêng Queries

---
+++ Quản Lý Sinh VIên/SQL Server Quan Ly Sinh Viên/Queries.sql
@@ -0,0 +1,27 @@
+--9 Create queries
+
+--a)
+select Ten
+from SVien
+where MaKhoa = 'CNTT'
+go
+
+--b)
+select TENMH, TINCHI
+from MHOC
+go
+
+--c)
+select *
+from KQua
+where MaSV = 8
+go
+
+--d)
+select sv.Ten, kq.MaHP
+from KQua kq, SVien sv
+where sv.MaSV = kq.MaSV
+and kq.Diem > 7
+go
+
+--e)
