f1433f5cc938a4a78b054c82098b2b719cec8f48
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2022-04-14 15:18:48 +0700

    Uppercase syntax keywords

--- Quản Lý Sinh VIên/SQL Server Quan Ly Sinh Viên/CreateDB.sql
+++ Quản Lý Sinh VIên/SQL Server Quan Ly Sinh Viên/CreateDB.sql
@@ -1,8 +1,8 @@
 create database MSTUDENT	
-go
+GO
 
 use MSTUDENT
-go
+GO
 
 create table KHOA
 (
@@ -12,7 +12,7 @@ NAMTHANHLAP int,
 constraint PK_Khoa primary key(MAKHOA),
 constraint UN_TENKHOA unique (TENKHOA)
 )
-go
+GO
 
 create table SVIEN
 (
@@ -21,10 +21,10 @@ TEN nvarchar(30),
 NAM int,
 MAKHOA char(4),
 constraint PK_SVien primary key(MASV),
-constraint CK_SVien check(NAM in (1, 2, 3, 4)), -- constraint CK_NAM check (nam > 0 and nam < 5),
+constraint CK_SVien check(NAM in (1, 2, 3, 4)), -- constraint CK_NAM check (nam > 0 AND nam < 5),
 constraint FK_SVien_Khoa foreign key (MAKHOA) references Khoa(MAKHOA)
 )
-go
+GO
 
 create table MHOC
 (
@@ -36,7 +36,7 @@ constraint PK_MonHoc primary key(MAMH),
 constraint UN_TENMH unique (TENMH),
 constraint FK_MonHoc_Khoa foreign key(MAKHOA) references Khoa(MAKHOA)
 )
-go
+GO
 
 create table DKIEN
 (
@@ -45,7 +45,7 @@ MAMH char(8),
 constraint PK_DKien foreign key(MAMH_TRUOC) references MHOC(MAMH),
 constraint FK_DKien_MonHoc foreign key(MaMH) references MHOC(MAMH)
 )
-go
+GO
 
 create table HPHAN
 (
@@ -57,7 +57,7 @@ GV nvarchar(30),
 constraint PK_HPhan primary key(MAHP),
 constraint FK_HOCPHAN_MONHOC foreign key (MAMH) references MHOC(MAMH)
 )
-go
+GO
 
 create table KQUA
 (
@@ -67,85 +67,85 @@ DIEM float,
 constraint PK_KQua primary key(MASV,MAHP),
 constraint FK_KQua_SVien foreign key(MASV) references SVIEN(MASV),
 constraint FK_KQua_HPhan foreign key(MAHP) references HPHAN(MAHP),
-constraint CK_KQua check(Diem > 0 and Diem < 11)
+constraint CK_KQua check(Diem > 0 AND Diem < 11)
 )
-go
+GO
 
-insert KHOA
-values
+INSERT KHOA
+VALUES
 ('CNTT', N'Cong nghe thong tin', 1995),
 ('VL', N'Vat Ly', 1976),
 ('TOAN', N'Toan', 1976)
-go
+GO
 
-insert SVIEN
-values
-(17, N'S?n', 1,'CNTT'),
-(8, N'B?o', 2, 'CNTT'),
+INSERT SVIEN
+VALUES
+(17, N'Son', 1,'CNTT'),
+(8, N'Bao', 2, 'CNTT'),
 (5,N'Trang', 3, 'TOAN')
-go
+GO
 
-insert MHOC
-values
+INSERT MHOC
+VALUES
 ('COSC1310', N'Nhap mon cong nghe thong tin', 4, 'CNTT' ),
 ('COSC3320', N'Cau truc du lieu', 4, 'CNTT'),
 ('MATH2410', N'Toan roi rac', 3, 'TOAN'),
 ('COSC3380', N'Co so du lieu', 3, 'CNTT'),
 ('PHYS3332', N'Vat ly dai cuong', 3, 'VL')
-go
+GO
 
-insert into DKIEN (MAMH, MAMH_TRUOC)
-values
+INSERT into DKIEN (MAMH, MAMH_TRUOC)
+VALUES
 ('COSC3380', 'COSC3320'),
 ('COSC3380', 'MATH2410'),
 ('COSC3320', 'COSC1310')
-go
+GO
 
 -- Insert data for 9g
-insert into DKIEN (MAMH, MAMH_TRUOC)
-values
+INSERT into DKIEN (MAMH, MAMH_TRUOC)
+VALUES
 ('COSC3320', 'COSC3380')
-go
+GO
 
-insert HPHAN
-values
+INSERT HPHAN
+VALUES
 ( 85, 'MATH2410', 1, 1996, N'Kim'),
 ( 92, 'COSC1310', 1, 1996, N'An'),
 (102, 'COSC3320', 2, 1997, N'Nhien'),
 (112, 'MATH2410', 1, 1997, N'Van'),
 (119, 'COSC1310', 1, 1997, N'An'),
 (135, 'COSC3380', 1, 1997, N'Son')
-go
+GO
 
-insert KQUA
-values
+INSERT KQUA
+VALUES
 (17, 102, 8),
 (17, 119, 6),
 (5, 85, 10),
 (8, 92, 10),
 (8, 102, 8),
 (8, 135, 10)
-go
+GO
 
 --5
-insert SVIEN
-values
+INSERT SVIEN
+VALUES
 (25, N'Nam',2,'CNTT')
-go
+GO
 
 --6
-insert KQUA
-values
+INSERT KQUA
+VALUES
 (25, 102, 7),
 (25, 135, 9)
-go
+GO
 
 --7
-update KQUA
-set Diem = 9
-where MAHP = 102
-and MASV = 8
-go
+UPDATE KQUA
+SET Diem = 9
+WHERE MAHP = 102
+AND MASV = 8
+GO
 
 SELECT DIEM, MAHP, MASV
 FROM KQUA
@@ -153,13 +153,14 @@ WHERE MASV = 8
 AND MAHP = 102
 
 --8
-delete from KQUA
-where MASV = 8
-and MAHP = 135
-and DIEM = 10
-go
+DELETE FROM KQUA
+WHERE MASV = 8
+AND MAHP = 135
+AND DIEM = 10
+GO
+
 SELECT *
 FROM KQUA
 WHERE MASV = 8
-and MAHP = 135
-and DIEM = 10
+AND MAHP = 135
+AND DIEM = 10
