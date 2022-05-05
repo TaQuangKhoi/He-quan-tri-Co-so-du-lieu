63e6253a288ab2f3880844ce9eac1a405c97a040
Keios Starqua <khoi0941@gmail.com>
2022-05-05 13:37:24 +0700

    Tạo PROC SV_by_KetQua

---
+++ Quản Lý Sinh VIên/MSTUDENT/PROC_FROM_QUERY.sql
@@ -0,0 +1,10 @@
+--d) Tim hoc sinh theo ket qua cuoi khoa nhap vao
+create proc SV_by_KetQua(
+    @diem float
+)
+as
+select S.TEN, K.DIEM, H.MAMH
+from KQUA K, HPHAN H, SVIEN S
+where K.MAHP = H.MAHP
+AND K.MASV = S.MASV
+AND K.DIEM > @diem
